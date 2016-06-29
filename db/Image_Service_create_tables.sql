4e16bf222edd01e66d61dcce8126a7f9662c8c31
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-29 17:23:13 +0200

    Code regeneration/Model synchronization

--- db/Image_Service_create_tables.sql
+++ db/Image_Service_create_tables.sql
@@ -1,13 +1,13 @@
 --
--- Database Schema:  test 
+-- Database Schema:  abc 
 -- Automatically generated sql script for the service Image Service, created by the CAE.
 -- --------------------------------------------------------
 
 --
--- Table structure for table tset.
+-- Table structure for table abc.
 --
-CREATE TABLE `test.tset` (
-  test int ,
-CONSTRAINT test_PK PRIMARY KEY (test)
+CREATE TABLE `abc.abc` (
+  id int ,
+CONSTRAINT id_PK PRIMARY KEY (id)
 );
 

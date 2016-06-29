ba4e4bf7ea4f76f8bf1c729e9a4f2dcfdbf0806c
CAE-CodeEditor-User <th_winkler@outlook.de>
2016-06-29 16:37:07 +0200

    Code regeneration/Model synchronization

--- db/Image_Service_create_tables.sql
+++ db/Image_Service_create_tables.sql
@@ -12,3 +12,17 @@ CREATE TABLE cae-schema.images (
 CONSTRAINT id_PK PRIMARY KEY (id)
 );
 
+--
+-- Database Schema:  cae-schema 
+-- Automatically generated sql script for the service Image Service, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table images.
+--
+CREATE TABLE cae-schema.images (
+  url varchar(255) ,
+  id int NOT NULL AUTO_INCREMENT,
+CONSTRAINT id_PK PRIMARY KEY (id)
+);
+
